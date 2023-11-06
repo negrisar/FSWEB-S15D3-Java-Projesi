805a0558be547bb6873bee416a4e50da12428f31
Nergis <armaganergis@gmail.com>
2023-11-06 22:15:53 +0300

    bitti

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,32 @@
+
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        List<Employee> employees = new LinkedList<>();
+
+        employees.add(new Employee(41, "negris", "armagan" ));
+        employees.add(new Employee(26, "arif", "ercik" ));
+        employees.add(new Employee(41, "negris", "armagan" ));
+
+        LinkedList<Employee> duplicateEmployees = new LinkedList<>();
+        Map<Integer, Employee> employeeMap = new HashMap<>();
+
+        for (Employee employee : employees) {
+            if (!employeeMap.containsKey(employee.getId())) {
+                employeeMap.put(employee.getId(), employee);
+            } else {
+                duplicateEmployees.add(employee);
+            }
+        }
+
+        System.out.println(employees);
+        System.out.println(employeeMap);
+        System.out.println("REMOVED" + duplicateEmployees);
+
+
     }
 }
